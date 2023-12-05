b26f1de66dc10331dd8bb7379d422450ea6cb081
Iván Pereira Muñoz <elchininet@gmail.com>
2023-12-05 02:53:22 +0100

    Remove unused constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,3 @@ export const HOST_SELECTOR = ':host';
 export const INVALID_SELECTOR = 'invalid selector';
 export const DEFAULT_RETRIES = 10;
 export const DEFAULT_DELAY = 10;
-export const ELEMENT_PROP = 'element';
