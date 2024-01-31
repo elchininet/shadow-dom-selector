765fe66b8c418d1079e376efa273a24f00d6ad99
Iván Pereira <elchininet@gmail.com>
2024-01-31 01:06:02 +0100

    Migrate from Cypress to Playwright (#35)
    * Migrate from Cypress to Playwright
    
    * Install Playwright
    
    * Install only Chromium

--- demo/demo.ts
+++ demo/demo.ts
@@ -2,6 +2,17 @@ import * as ShadowDomSelector from '../src';
 
 // @ts-ignore
 window.ShadowDomSelector = ShadowDomSelector;
+// @ts-ignore
+window.__compare = <T extends Node = Element>(
+    nodeList1: NodeListOf<T>,
+    nodeList2: NodeListOf<T>
+): boolean => {
+    const sameLength = nodeList1.length === nodeList2.length;
+    const sameElements = Array.from(nodeList1).every((element: T, index: number) => {
+        return element === nodeList2.item(index);
+    });
+    return sameLength && sameElements;
+};
 
 class DelayedListContainer extends HTMLElement {
     constructor() {
--- tests/global.ts
+++ tests/global.ts
@@ -3,5 +3,6 @@ import * as ShadowDomSelector from '../src';
 declare global {
   interface Window {
     ShadowDomSelector: typeof ShadowDomSelector;
+    __compare: <T extends Node = Element>(nodeList1: NodeListOf<T>, nodeList2: NodeListOf<T>) => boolean;
   }
 }
